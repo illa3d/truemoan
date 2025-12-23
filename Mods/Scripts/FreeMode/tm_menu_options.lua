f62c0647777bbb86d272ba5c43033daa19d54785
illa3d <illa3d@gmail.com>
2025-12-23 06:00:17 +0100

    Menu - Options shuffle (handy hide/show ui on double click)

--- Mods/Scripts/FreeMode/tm_menu_options.lua
+++ Mods/Scripts/FreeMode/tm_menu_options.lua
@@ -2,35 +2,19 @@
 adec = 1
 
 label TMMenuOptions(human)
-	+ "Music.." [gold]
-		+ MenuBack
-			Return (2)
-		+ for i, track in ipairs(MusicTracks)
-			+ "Play " .. track
-				Music(track , 0.05, 0)
-				Return()
-		+ "Stop Music"
-			StopMusic()
-			Return()
-		+ MenuBack
-			Return(2)
-		+ MenuClose
-	+ "Ambience.. | " .. BoolLabel(AllowAmbience) [gold]
-		+ "Refresh" [if AllowAmbience == true]
-			Return()
-		+ "Next track | " .. WordLabel("Ambience " .. tmAmbienceTrack) [if AllowAmbience]
-			TMPlayAmbienceNext()
-			Return()
-		+ "Ambience | " .. BoolLabel(AllowAmbience) .. " (loops in ".. ValueLabel(TMAmbienceLeftSec()) .. "s)" [if AllowAmbience == true]
-			AllowAmbience = false
-			Return()
-		+ "Ambience | " .. BoolLabel(AllowAmbience) [if AllowAmbience == false]
-			AllowAmbience = true
-			TMPlayAmbienceCurrent()
-			Return()
-		+ MenuBack
-			Return(2)
-		+ MenuClose
+	+ "Hide UI" [if HG_UIvis == true]
+		HG_UIvis = false
+		game.m_controlsUI.Show(false)
+	+ "Show UI" [if HG_UIvis == false]
+		HG_UIvis = true
+		game.m_controlsUI.Show()
+		Return()
+	+ "New people | " .. WordLabel("Naked") [if NakedOnSpawn == true]
+		NakedOnSpawn = false
+		Return()
+	+ "New people | " .. WordLabel("Clothed") [if NakedOnSpawn == false]
+		NakedOnSpawn = true
+		Return()
 	-- + "Sex.." [gold] --return when options fill up
 	+ "Moan Sex | " .. BoolLabel(Moaning) [if Moaning == true]
 		Moaning = false
@@ -53,19 +37,35 @@ label TMMenuOptions(human)
 	-- + MenuBack
 	-- 	Return(2)
 	-- + MenuClose
-	+ "New people | " .. WordLabel("Naked") [if NakedOnSpawn == true]
-		NakedOnSpawn = false
-		Return()
-	+ "New people | " .. WordLabel("Clothed") [if NakedOnSpawn == false]
-		NakedOnSpawn = true
-		Return()
-	+ "Hide UI" [if HG_UIvis == true]
-		HG_UIvis = false
-		game.m_controlsUI.Show(false)
-	+ "Show UI" [if HG_UIvis == false]
-		HG_UIvis = true
-		game.m_controlsUI.Show()
-		Return()
+	+ "Ambience.. | " .. BoolLabel(AllowAmbience) [gold]
+		+ "Refresh" [if AllowAmbience == true]
+			Return()
+		+ "Next track | " .. WordLabel("Ambience " .. tmAmbienceTrack) [if AllowAmbience]
+			TMPlayAmbienceNext()
+			Return()
+		+ "Ambience | " .. BoolLabel(AllowAmbience) .. " (loops in ".. ValueLabel(TMAmbienceLeftSec()) .. "s)" [if AllowAmbience == true]
+			AllowAmbience = false
+			Return()
+		+ "Ambience | " .. BoolLabel(AllowAmbience) [if AllowAmbience == false]
+			AllowAmbience = true
+			TMPlayAmbienceCurrent()
+			Return()
+		+ MenuBack
+			Return(2)
+		+ MenuClose
+	+ "Music.." [gold]
+		+ MenuBack
+			Return (2)
+		+ for i, track in ipairs(MusicTracks)
+			+ "Play " .. track
+				Music(track , 0.05, 0)
+				Return()
+		+ "Stop Music"
+			StopMusic()
+			Return()
+		+ MenuBack
+			Return(2)
+		+ MenuClose
 	+ MenuBack
 		Return(2)
 	+ MenuClose
