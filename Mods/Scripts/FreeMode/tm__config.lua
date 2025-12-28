89d5a9499510383aed31216578f3347ea26594c9
illa3d <illa3d@gmail.com>
2025-12-28 01:06:57 +0100

    Config formatting

--- Mods/Scripts/FreeMode/tm__config.lua
+++ Mods/Scripts/FreeMode/tm__config.lua
@@ -1,57 +1,54 @@
 -- TrueMoan v1.3 by illa3d
 ------------------------------------------------------------------------------------------------------------------
--- TRUE MOAN DEFAULTS CONFIG
--- These values are used every time new scene loads
+-- TRUE MOAN DEFAULTS CONFIG (Values changed in options reset after scene load, here you set that permanent)
 ------------------------------------------------------------------------------------------------------------------
 --                              !!! REMEMBER TO BACKUP BEFORE UPDATING THE MOD !!!                              --
 --     Some values will be added through time. If you just copy your backup, you might be losing on options!    --
 ------------------------------------------------------------------------------------------------------------------
--- MAIN DEFAULTS
-------------------------------------------------------------------------------------------------------------------
 
 -- Menu
-TM_Menu_FaunaLabs = false         -- Display Fauna labs Maoin Menu (if FaunaLABS is present)
-TM_Menu_HideShowUI = false        -- Display Show/hide UI option in Main Menu
-TM_MenuBackButtonEvery = 20       -- [5-20] After how many items back button appear in item lists
+TM_Menu_FaunaLabs = false			-- 1. Display Fauna labs Maoin Menu (if FaunaLABS is present)
+TM_Menu_HideShowUI = false			-- 2. Display Show/hide UI option in Main Menu
+TM_MenuBackButtonEvery = 20			-- 3. [5-20] After how many items back button appear in item lists
 
 -- Sex Menu
-TM_ShowSexStyleControl = true     -- Show/Hide SexControl/Style controls (Male/Thrust) in the menu
+TM_ShowSexStyleControl = true		-- 4. Show/Hide SexControl/Style controls (Male/Thrust) in the menu
 
 -- Double click
-TM_DoubleClickInOtherMods = false -- Allows double clicking in other mods (works only with TMMM stickied option)
-TM_DoubleClickReset = true        -- Reset character pose/activity/face/animations on double click
-TM_DoubleClickUndress = true      -- Take clothes off on double click
-TM_DoubleClickMoan = true         -- Moan on dobuleclick (doesn't work with VoiceMod)
-TM_DoubleClickDelay = 0.25          -- [0.2-0.4] Time to register double click (0.25 = 250ms, 200-400ms is fine)
+TM_DoubleClickInOtherMods = false	-- 5. Allows double clicking in other mods (works only with TMMM stickied option)
+TM_DoubleClickReset = true			-- 6. Reset character pose/activity/face/animations on double click
+TM_DoubleClickUndress = true		-- 7. Take clothes off on double click
+TM_DoubleClickMoan = true			-- 8. Moan on dobuleclick (doesn't work with VoiceMod)
+TM_DoubleClickDelay = 0.25			-- 9. [0.2-0.4] Time to register double click (0.25 = 250ms, 200-400ms is fine)
 
 -- People
-TM_SpawnNaked = false             -- Characters naked when spawned / changing
-TM_SpawnReset = false             -- Characters reset when spawned / changing (no sucking lol)
-TM_SpawnNoFuta = false            -- Females spawn without penises (no sucking lol)
-TM_MoanSex = true                 -- Moaning during sex
-TM_WetSex = true                  -- Moisting holes (vag, anus, mouth)
+TM_SpawnNaked = false				-- 10. Characters naked when spawned / changing
+TM_SpawnReset = false				-- 11. Characters reset when spawned / changing (no sucking lol)
+TM_SpawnNoFuta = false				-- 12. Females spawn without penises (no sucking lol)
+TM_MoanSex = true					-- 13. Moaning during sex
+TM_WetSex = true					-- 14. Moisting holes (vag, anus, mouth)
 
 -- Body Edit
-TM_BodyEditSafe = true            -- Turns on safe body editing using SafeMin values
-TM_BreastSafeMin = -0.8           -- Some characters fall apart below this value and game crashes
+TM_BodyEditSafe = true				-- 15. Turns on safe body editing using SafeMin values
+TM_BreastSafeMin = -0.8				-- 16. Some characters fall apart below this value and game crashes
 
 -- SFX Ambience
-TM_AllowAmbience = true           -- Ambient sounds
-TM_AmbienceVolume = 0.7           -- [0-1] Ambient volume
+TM_AllowAmbience = true				-- 17. Ambient sounds
+TM_AmbienceVolume = 0.7				-- 18. [0-1] Ambient volume
 
 ------------------------------------------------------------------------------------------------------------------
 -- FINE TUNING
 ------------------------------------------------------------------------------------------------------------------
 
 -- Body Edit Increments
-TM_BodyStepA = 0.1   -- nipples hip, waist, muscle
-TM_BodyStepB = 0.05  -- ass, penis-length, penis-size, forearm, upperarm, calf, thigh, neck
-TM_BodyStepC = 0.025 -- breast, body
+TM_BodyStepA = 0.1		-- nipples hip, waist, muscle
+TM_BodyStepB = 0.05		-- ass, penis-length, penis-size, forearm, upperarm, calf, thigh, neck
+TM_BodyStepC = 0.025	-- breast, body
 
-TM_SexSpeedStep = 0.05         -- non-linear percent (increments: slow = small, fast = big)
-TM_SexMaleStep = 0.1           -- giver/getter, linear percent (of UI slider)
-TM_SexThrustStep = 0.1         -- easier/harder, linear percent (of UI slider)
-TM_SexDepthStep = 0.05         -- shallow/deep, linear percent (of UI slider)
+TM_SexSpeedStep = 0.05	-- non-linear percent (increments: slow = small, fast = big)
+TM_SexMaleStep = 0.05	-- giver/getter, linear percent (of UI slider)
+TM_SexThrustStep = 0.05	-- easier/harder, linear percent (of UI slider)
+TM_SexDepthStep = 0.05	-- shallow/deep, linear percent (of UI slider)
 
 -- Sex speed (Min: 0.001, Max: 2)
 TM_SexSpeedMax = 2
@@ -68,7 +65,7 @@ TM_FastTreshold = 0.3
 TM_NormalTreshold = 0.1
 
 -- Moan Cum frequency (sec)
-TM_MoanCumEyeTime = 1
-TM_MoanCumLipsTime = 3
-TM_MoanCumBodyTime = 5
-TM_WetnessStep = 200
+TM_MoanCumEyeTime = 1	-- Cum in eye
+TM_MoanCumLipsTime = 3	-- Cum on lips
+TM_MoanCumBodyTime = 5	-- Cum on body
+TM_WetnessStep = 200	-- Wetness Control step
