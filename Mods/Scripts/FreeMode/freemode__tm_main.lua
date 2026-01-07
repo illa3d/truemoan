81486f129c0b1c1cf5ba92faca80a611f94c9679
illa3d <illa3d@gmail.com>
2026-01-07 15:20:30 +0100

    Added freemode__tm_main.lua for startups without freemode_main.lua

---
+++ Mods/Scripts/FreeMode/freemode__tm_main.lua
@@ -0,0 +1,8 @@
+-- TrueMoan startup fix. Both original freemode_main.lua and common.lua are required to start
+-- This file is just a fix in case freemode_main.lua is missing.
+-- If commons.lua is missing, download both original scripts: TrueFacials v0.59_ORIGINAL_SCRIPTS_FIX.zip
+-- https://drive.google.com/drive/folders/1XMsyMxDT6qCclCX4SPoU6H769uouaD6c
+-- Just extract to game folder
+
+label Start()
+stop
