3cdb3e9fd046ee5187eb97732e6c3eac73eefcfb
illa3d <illa3d@gmail.com>
2026-01-29 00:09:07 +0100

    Addons - AllVoiceModsConfig

---
+++ Addons/TrueMoan_Addon_VoicePack_AllVoiceMods/Mods/Scripts/FreeMode/tm_voice__allvoicemods_config.lua
@@ -0,0 +1,125 @@
+-- TrueMoan v2.6 by illa3d
+-- Add specific Voice Pack to a character by default
+-- 1. Find out EXACT voice name in VoicePack script
+--    EXAMPLE: tm_voice_chiyoru.lua - TMVoiceAdd({ Name = "Chiyoru", ...
+-- 2. Enter this specific name below for characters you want to use that voice pack
+--    EXAMPLE: 	TM_Voices_Set["android 18"] = "Chiyoru"
+
+-- IF A VOICE IS MISSING, FEEL FREE TO ADD A NEW CHARACTER = VOICE assignment
+-- Character name is filename in TrueFacials\Mods\Characters (letter case matters!)
+--   EXAMPLE: TrueFacials\Mods\Characters\judy alvarez.tfa - "judy alvarez"
+
+if type(TM_Voices_Character) == "table" then
+	TM_Voices_Character["2b"]					= ""
+	-- A
+	TM_Voices_Character["ada wong"]				= "(vm) Ada"
+	TM_Voices_Character["aerith"]				= "(vm) Aerith"
+	TM_Voices_Character["ahri kda"]				= ""
+	TM_Voices_Character["akali"]				= ""
+	TM_Voices_Character["android 18"]			= ""
+	TM_Voices_Character["ashley"]				= "(vm) Ashley"
+	TM_Voices_Character["aunt cass"]			= "(vm) Auntcass"
+	-- B
+	TM_Voices_Character["bayonetta"]			= "(vm) Bayonetta"
+	TM_Voices_Character["black cat"]			= "(vm) Blackcat"
+	TM_Voices_Character["black male"]			= ""
+	-- C
+	TM_Voices_Character["cammy"]				= "(vm) Cammy"
+	TM_Voices_Character["cheri nowlin"]			= ""
+	TM_Voices_Character["chunli"]				= ""
+	TM_Voices_Character["cindy aurum"]			= ""
+	TM_Voices_Character["ciri"]					= "(vm) Ciri"
+	TM_Voices_Character["cloud"]				= ""
+	-- D
+	TM_Voices_Character["dante"]				= ""
+	TM_Voices_Character["dimitrescu"]			= ""
+	TM_Voices_Character["dva"]					= "(vm) DVa"
+	-- E
+	TM_Voices_Character["elizabethbas"]			= "(vm) Elizabethyoung(deep)"
+	TM_Voices_Character["elizabethyoung"]		= "(vm) Elizabethyoung"
+	TM_Voices_Character["ellie"]				= "(vm) Ellie"
+	TM_Voices_Character["emilie de rochefort"]	= ""
+	TM_Voices_Character["eva"]					= ""
+	TM_Voices_Character["eve"]					= "(vm) Eve"
+	TM_Voices_Character["eve_xmas"]				= "(vm) Eve(deep)"
+	-- F
+	TM_Voices_Character["faye valentine"]		= "(vm) Faye"
+	-- G
+	TM_Voices_Character["gemma"]				= "(vm) Gemma"
+	TM_Voices_Character["generic male"]			= ""
+	TM_Voices_Character["geralt"]				= ""
+	-- H
+	TM_Voices_Character["hailey"]				= ""
+	TM_Voices_Character["harley quinn"]			= ""
+	TM_Voices_Character["honoka"]				= ""
+	-- I
+	TM_Voices_Character["ifalna"]				= ""
+	TM_Voices_Character["invisible woman"]		= ""
+	TM_Voices_Character["irelia"]				= ""
+	TM_Voices_Character["ivy"]					= "(vm) Ivy"
+	-- J
+	TM_Voices_Character["jessie rasberry"]		= "(vm) Jessie"
+	TM_Voices_Character["jill valentine"]		= "(vm) Jill"
+	TM_Voices_Character["jinx"]					= ""
+	TM_Voices_Character["judy alvarez"]			= ""
+	TM_Voices_Character["juliet"]				= ""
+	TM_Voices_Character["juri sf6"]				= "(vm) Juri(deep)"
+	TM_Voices_Character["juri"]					= "(vm) Juri"
+	-- K
+	TM_Voices_Character["kitana"]				= "(vm) Kitana"
+	-- L
+	TM_Voices_Character["lara croft"]			= "(vm) Lara"
+	TM_Voices_Character["leon s kennedy"]		= "(vm) Leon"
+	TM_Voices_Character["lily artemis"]			= "(vm) Lili"
+	TM_Voices_Character["link warriors"]		= "(vm) Link"
+	TM_Voices_Character["loba"]					= ""
+	TM_Voices_Character["lunafreya"]			= ""
+	-- M
+	TM_Voices_Character["maddie"]				= ""
+	TM_Voices_Character["mai shiranui"]			= "(vm) Mai"
+	TM_Voices_Character["marie rose"]			= ""
+	TM_Voices_Character["mercy"]				= "(vm) Mercy"
+	TM_Voices_Character["miss fortune"]			= ""
+	TM_Voices_Character["morrigan"]				= ""
+	TM_Voices_Character["mrx"]					= ""
+	-- N
+	TM_Voices_Character["nyotengu"]				= ""
+	-- O
+	-- P
+	TM_Voices_Character["panam"]				= ""
+	TM_Voices_Character["pharah"]				= "(vm) Pharah"
+	TM_Voices_Character["power"]				= "(vm) Power"
+	TM_Voices_Character["primrose office"]		= ""
+	TM_Voices_Character["primrose"]				= ""
+	-- Q
+	TM_Voices_Character["queen"]				= ""
+	TM_Voices_Character["quiet"]				= "(vm) Quiet"
+	-- R
+	TM_Voices_Character["rangiku"]				= "(vm) Rangiku"
+	TM_Voices_Character["raven teentitan"]		= "(vm) Raven(deep)"
+	TM_Voices_Character["raven"]				= "(vm) Raven"
+	-- S
+	TM_Voices_Character["samus"]				= ""
+	TM_Voices_Character["scarlet"]				= "(vm) Scarlet"
+	TM_Voices_Character["shadowheart"]			= ""
+	TM_Voices_Character["succubus"]				= ""
+	-- T
+	TM_Voices_Character["tachy"]				= ""
+	TM_Voices_Character["tifa lifeguard"]		= "(vm) Tifa(deep)"
+	TM_Voices_Character["tifa"]					= "(vm) Tifa"
+	TM_Voices_Character["tifa_kimono"]			= ""
+	TM_Voices_Character["tracer"]				= "(vm) Tracer"
+	TM_Voices_Character["triss"]				= ""
+	-- U
+	TM_Voices_Character["ultimate bunny"]		= ""
+	-- W
+	TM_Voices_Character["widowmaker"]			= "(vm) Widowmaker"
+	TM_Voices_Character["witch mercy"]			= ""
+	-- X
+	-- Y
+	TM_Voices_Character["y'shtola"]				= ""
+	TM_Voices_Character["yennefer"]				= ""
+	TM_Voices_Character["yuffie"]				= "(vm) Yuffie"
+	-- Z
+	TM_Voices_Character["zelda"]				= "(vm) Zelda"
+end
