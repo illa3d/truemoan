fe5044ba5b39c3344a436f85cc560ebc7d2208f7
illa3d <illa3d@gmail.com>
2026-01-31 16:13:21 +0100

    VoicePack - Chiyoru moved as default

---
+++ Mods/Scripts/FreeMode/tm_voice__default.lua
@@ -0,0 +1,9 @@
+-- True Moan Voice Pack
+-- See tm_voice_README.txt for details
+TMVoiceAdd({
+	Name = "Chiyoru",
+	-- No remap needed, all tier audio files present
+	[TMSfx.Plap] =		{ Files = 20, Volume = 0.7 },
+	[TMSfx.Suck] =		{ Files = 25, Volume = 0.9 },
+	[TMSfx.SuckDeep] =	{ Files = 25, Volume = 0.9 },
+})
