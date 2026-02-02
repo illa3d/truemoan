a39cb976a417e684c0f60d872aead8306e7652a5
illa3d <illa3d@gmail.com>
2026-02-02 22:56:18 +0100

    Audio - fix sfx fallback

--- Mods/Scripts/FreeMode/tm_audio.lua
+++ Mods/Scripts/FreeMode/tm_audio.lua
@@ -69,7 +69,7 @@ TMVoiceDefault = {
 }
 
 TMSfxDefault = {
-	Name = "Chiyoru",
+	Name = "TrueMoan",
 	[TMSfx.Fart] = { Files = 7, Volume = 0.5 },
 	[TMSfx.Plap] = { Files = 20, Volume = 0.7 },
 	[TMSfx.Suck] = { Files = 25, Volume = 0.9 },
