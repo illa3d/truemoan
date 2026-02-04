4e2b5a340e182caf35c41bef9fd1a83d770f21a7
illa3d <illa3d@gmail.com>
2026-02-04 12:23:16 +0100

    VoicePacks - Function check

--- Mods/Scripts/FreeMode/tm_voice_template.lua
+++ Mods/Scripts/FreeMode/tm_voice_template.lua
@@ -1,10 +1,10 @@
 -- True Moan Voice Pack
 -- See tm_voice_README.txt for details
-TMVoiceAdd({
+if type(TMVoiceAdd) == "function" then TMVoiceAdd({
 	-- VOICE PACK NAME (can have spaces, can have upper-cases. Files must all be lowercase!)
 	Name = "",
 	-- TIER REMAPPING (leave this in if the following tiers don't have audio files)
 	[TMTier.Faster] = TMTier.Fast,
 	[TMTier.Wild] = TMTier.Fast,
 	[TMTier.Max] = TMTier.Fast,
-})
+}) end
