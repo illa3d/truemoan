c747180fe1f09c805983b1e1bd788e331249c3be
illa3d <illa3d@gmail.com>
2026-02-09 22:50:37 +0100

    VoicePack DefaultMale

---
+++ Mods/Scripts/FreeMode/tm_voice__defaultmale.lua
@@ -0,0 +1,10 @@
+-- True Moan Voice Pack
+-- See tm_voice_README.txt for details
+TMVoiceAdd({
+	Name = "Leon",
+	IsMale = true,
+	-- TIER REMAPPING (leave this in if the following tiers don't have audio files)
+	[TMTier.Faster] = TMTier.Fast,
+	[TMTier.Wild] = TMTier.Fast,
+	[TMTier.Max] = TMTier.Fast,
+})
